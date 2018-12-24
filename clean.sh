89c287b69407b3300b53fa5de38a84fd3fcf9464
angel <angel.imaz@hush.com>
2018-12-24 19:10:37 +0100

    first commit

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+CP="lib/*:classes"
+SP=src/java/
+APPLICATION="shr"
+
+/bin/rm -f ${APPLICATION}.jar
+/bin/rm -f ${APPLICATION}service.jar
+/bin/rm -rf classes
+/bin/mkdir -p classes/
+/bin/rm -rf addons/classes
+/bin/mkdir -p addons/classes/
+
